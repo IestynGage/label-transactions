e37b18b4d98ffbe0ef8af2dad322e52b05db74c1
Iestyn <33298011+IestynGage@users.noreply.github.com>
2026-08-06 08:31:55 +0100

    Change if statement to match/case for readability
    I want to add a new type of input which finds the new/deleted transactions.

--- main.py
+++ main.py
@@ -7,19 +7,23 @@ from rules import Rules, filter_by_month, validate_label
 from tui.CSVCategorizer import CsvCategorizer
 from tui.ExtracMonth import MonthSelectorApp
 from tui.existing import continue_with_existing
+from reprocess import process_new_or_deleted_transaction
 
 def main():
+    transactions:list[Transaction] = []
     existing_transactions:list[Transaction] = parse_excel('transactions.xlsx')
     validate_label()
 
-    transactions:list[Transaction] = []
-    if continue_with_existing(existing_transactions):
-        transactions = existing_transactions
-    else:
-        csv_files = CsvCategorizer().run()
-        month = MonthSelectorApp(csv_files).run().split()[0]
-        csv_transactions = parse_csv_files(csv_files)
-        transactions = filter_by_month(csv_transactions, month)
+    match continue_with_existing(existing_transactions):
+        case 're-label':
+            transactions = existing_transactions
+        case 'new':
+            csv_files = CsvCategorizer().run()
+            month = MonthSelectorApp(csv_files).run().split()[0]
+            csv_transactions = parse_csv_files(csv_files)
+            transactions = filter_by_month(csv_transactions, month)
+        case 'cancel':
+            return;
     
     labeled_transactions = Rules().apply(transactions)
     export_transactions(labeled_transactions)
